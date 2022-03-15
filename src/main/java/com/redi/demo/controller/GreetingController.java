bc4786dee5e120b27b289d4f68153a4bc92988fd
odfsoft <folgerfonseca@gmail.com>
2022-03-15 21:03:50 +0100

    greeting with request param

--- src/main/java/com/redi/demo/controller/GreetingController.java
+++ src/main/java/com/redi/demo/controller/GreetingController.java
@@ -1,13 +1,14 @@
 package com.redi.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
     @GetMapping("greeting")
-    public String greet() {
-        return "Hello, World!";
+    public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
     }
 }
