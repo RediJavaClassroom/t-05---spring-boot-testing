6f3a474d427df94e0d0b18e881f7b4c5312eb3a2
odfsoft <folgerfonseca@gmail.com>
2022-03-29 21:04:09 +0200

    add greeting service to contorller

--- src/main/java/com/redi/demo/controller/GreetingController.java
+++ src/main/java/com/redi/demo/controller/GreetingController.java
@@ -2,6 +2,8 @@ package com.redi.demo.controller;
 
 import com.redi.demo.domain.Greeting;
 import com.redi.demo.domain.GreetingStatistic;
+import com.redi.demo.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,28 +17,20 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private final AtomicLong counter = new AtomicLong();
+    GreetingService greetingService;
 
-    private final Map<String, Long> statistics = new HashMap<>();
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     @GetMapping("greeting")
     public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
-        storeGreetStatistic(name);
-        return new Greeting(counter.incrementAndGet(), "Hello, " + name + "!");
-    }
-
-    private void storeGreetStatistic(String name) {
-        long greets = statistics.getOrDefault(name, 0L) + 1;
-        statistics.put(name, greets);
+        return greetingService.greet(name);
     }
 
     @GetMapping("statistics")
-    public List<GreetingStatistic> greet() {
-        List<GreetingStatistic> greetingStatistics = new ArrayList<>();
-        for(Map.Entry<String, Long> entry: statistics.entrySet()) {
-            GreetingStatistic greetingStatistic = new GreetingStatistic(entry.getKey(), entry.getValue());
-            greetingStatistics.add(greetingStatistic);
-        }
-        return greetingStatistics;
+    public List<GreetingStatistic> statistics() {
+        return greetingService.getStatistics();
     }
 }
